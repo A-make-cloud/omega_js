e175c9c1e41f38c7b6ae894650593b5478041356
A-make-cloud <68588824+A-make-cloud@users.noreply.github.com>
2023-12-12 22:14:29 +0100

    Update filtre.js

--- js/filtre.js
+++ js/filtre.js
@@ -1,86 +1,126 @@
-const filter = document.getElementById('filter')
-const listItems = []
+let storeItems = document.getElementById('store-items')
+let storeSeach = document.getElementById('store-search')
 
-getData()
-
-filter.addEventListener('input', (evt) => filterData(evt.target.value))
-
-async function getData() {
-  const storeItems = document.getElementById('store-items')
-
-  const displayResult = document.getElementById('result')
-  const res = await fetch('../catalogue.json')
-  /*
-     const data = await res.json()
-   //  Clear displayResult
-   displayResult.innerHTML = ''
- 
-   data.results
- */
-
-  //                 destructuration
-  const results = await res.json()
-  //  Clear result
-  displayResult.innerHTML = ''
-
-  results.forEach(wo => {
-    //   console.log(user);
-    const li = document.createElement('li')
-    listItems.push(li)
-    //  user.picture.large -> dans le api data
-
-    li.innerHTML += `
-    <div class="col-10 col-sm-6 col-lg-10 mx-auto my-3 store-item sweets item-list"
-            data-item="sweets">
-            <div class="card ">
-              <div class="../img-container search-card">
-                <img src=${wo.image} class="card-img-top store-img" 
-                  alt>
-                  <div> 
-                   <h5 id="store-item-name">${wo.name}</h5>
-                 
-                <h5 class="store-item-value"> <strong id="store-item-price"
-                    class="font-weight-bold">$ ${wo.price}</strong></h5>
-                    </div>
-                <span class="store-item-icon">
-                  <i class="fas fa-shopping-cart"></i>
-                </span>
-                  <div class="descr"><h6>Fiche Produit</h6><p>${wo.description} </p></div>
-              </div>
-              <div class="card-body-search">
-                <div
-                  class="card-text d-flex justify-content-between text-capitalize">
-
+function displayProds() {
+  prods.map((elem) => {
+    const { id, name, price, image, category } = elem;
+    storeItems.innerHTML += `
+      <div class="col-10 col-sm-6 col-lg-4 mx-auto my-3 store-item cakes"
+              data-item="${category}">
+              <div class="card item-card">
+                <div class="img-container">
+                  <img src=${image} class="card-img-top store-img"
+                    alt>
                 </div>
+                <div class='name-item'>
+                <h5 id="store-item-name">${name}</h5>
+                <h5 class="store-item-value"> <strong id="store-item-price"
+                    class="font-weight-bold">${price} €</strong></h5>
+            
+                
+                            <i class="fas fa-shopping-cart" class='store-item-icon add-product' onclick='addtocart(${id})'></i>
               </div>
-
+                <div class="card-body">
+                  <div
+                    class="card-text d-flex justify-content-between text-capitalize">
+  
+                  </div>
+                </div>
+  
+              </div>
+              <!-- end of card-->
             </div>
-            <!-- end of card-->
-          </div>
-    `;
-    displayResult.appendChild(li)
-  });
-}
-const itemList = document.querySelectorAll('.item-list')
-itemList.forEach(it => {
+      `;
 
-  it.style.display = 'none'
-})
-function filterData(searchTerm) {
-  console.log(searchTerm);
-  // listItems.style.display = 'none'
-  listItems.forEach(item => {
 
-    if (item.innerText.toLowerCase().includes(searchTerm.toLowerCase())) {
-      itemList.forEach(it => {
+    function filterMenu() {
+      const buttons = document.querySelectorAll('.filter-btn')
+      const storeImages = document.querySelectorAll('.store-item')
+      buttons.forEach((button) => {
+        button.addEventListener('click', (e) => {
+          e.preventDefault()
+          const filt = e.target.dataset.filter
+          storeImages.forEach(si => {
+            const categ = e.target.dataset.item
+            // console.log(filt);// categories sur laquelle on clique
+            if (filt === 'all') {
+              si.style.display = 'block'
 
-        it.style.display = 'block'
+              console.log('categories avant:', category);
+            } else if (filt === category) {
+              let outlet = ''
+
+                .filter(fi => {
+                  if (fi.category === filt) {
+                    outlet +=
+                      `
+                    <div class="col-10 col-sm-6 col-lg-4 mx-auto my-3 store-item cakes"
+                            data-item="${fi.category}">
+                            <div class="card ">
+                              <div class="../img-container">
+                                <img src=${fi.image} class="card-img-top store-img"
+                                  alt>
+                                <h5 id="store-item-name">${fi.name}</h5>
+                                <h5 class="store-item-value"> <strong id="store-item-price"
+                                    class="font-weight-bold">${fi.price}</strong></h5>
+                                <span class="store-item-icon">
+                                  <i class="fas fa-shopping-cart"></i>
+                                </span>
+                              </div>
+                              <div class="card-body">
+                                <div
+                                  class="card-text d-flex justify-content-between text-capitalize">
+                
+                                </div>
+                              </div>
+                
+                            </div>
+                            <!-- end of card-->
+                          </div>
+                    `
+                  }
+                  //return fi.category === filt
+                }).join('')
+              //    console.log('myCateg filter :', categProds);
+              storeItems.innerHTML = outlet
+            }
+          })
+        })
       })
-      item.classList.remove('hide')
-      // listItems.style.display = 'block'
-    } else {
-      item.classList.add('hide')
-
     }
+    filterMenu()
   })
 }
+displayProds()
+
+function searchItem() {
+  const searchItem = document.getElementById('search-item')
+  const typeItem = document.querySelector('#search-item')
+  let word = ''
+  typeItem.addEventListener('input', (evt) => {
+    let nameSearch = prods.map(wo => {
+      const { name, image } = wo
+      //   console.log(name);
+      evt.preventDefault()
+      let searchValue = evt.target.value;
+      //if (name.toLowerCase().startsWith('str')) {
+      if (name.toLowerCase().startsWith(searchValue)) {
+        word +=
+          `
+        <img src=${image} width="150px">
+       `
+      }
+      //word = name.toLowerCase().startsWith('ch')
+    })
+
+    storeSeach.innerHTML = word
+    // storeSeach.innerHTML = typeItem.value
+    console.log(typeItem.value);
+  })
+}
+searchItem()
+
+
+
+
+
